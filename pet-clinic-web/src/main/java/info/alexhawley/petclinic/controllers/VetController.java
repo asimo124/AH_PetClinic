e2daed70485251eeb175846dbc00e30ab8041157
asimo124 <asimo124@yahoo.com>
2019-04-29 14:14:19 -0500

    Finished adding Master Layout Page to both Owners List and Vets List.

---
+++ pet-clinic-web/src/main/java/info/alexhawley/petclinic/controllers/VetController.java
@@ -0,0 +1,31 @@
+package info.alexhawley.petclinic.controllers;
+
+
+import info.alexhawley.petclinic.services.VetService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jt on 7/22/18.
+ */
+@RequestMapping("/vets")
+@Controller
+public class VetController {
+
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
+
+        return "vets/index";
+    }
+}
+
+
